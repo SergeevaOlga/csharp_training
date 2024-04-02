592acdfcacfa05bcaf771a0058296bf5d64ad3ac
SergeevaOlga <freyaspb@mail.ru>
2024-04-03 00:52:31 +0300

    Лекция 1.5

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using addressbook_web_tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace WebAddressbookTests
@@ -7,8 +8,47 @@ namespace WebAddressbookTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+
+            Assert.IsTrue(s2.Colored);
+            Assert.IsFalse(s1.Colored);
+            Assert.IsFalse(s3.Colored);
+        }
+
+        [TestMethod]
+        public void TestMethodCircle()
+        {
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+
+            Assert.AreEqual(s1.Radius, 15);
+
+            s1.Colored = true;
+
+            Assert.IsTrue(s1.Colored);
+            Assert.IsTrue(s3.Colored);
+            Assert.IsFalse(s2.Colored);
         }
     }
 }
